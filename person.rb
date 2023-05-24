ad84e8aaa165a83ca5394b7aa869259a43bb24e4
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-05-24 23:31:16 +0430

    Add nameable.rb and decorators

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
-class Person
+# frozen_string_literal: true
+
+require_relative('./nameable')
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
@@ -7,6 +10,10 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
