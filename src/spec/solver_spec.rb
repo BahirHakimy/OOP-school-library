150225242f2bcdca11a3a0388140b67f68e487d2
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-02 00:37:29 +0430

    reorganized the code

--- src/spec/solver_spec.rb
+++ src/spec/solver_spec.rb
@@ -34,4 +34,30 @@ describe Solver do
       end
     end
   end
+
+  describe '#fizzbuzz' do
+    it "returns 'fizz' when N is divisible by 3" do
+      expect(subject.fizzbuzz(3)).to eq('fizz')
+      expect(subject.fizzbuzz(9)).to eq('fizz')
+      expect(subject.fizzbuzz(12)).to eq('fizz')
+    end
+
+    it "returns 'buzz' when N is divisible by 5" do
+      expect(subject.fizzbuzz(5)).to eq('buzz')
+      expect(subject.fizzbuzz(10)).to eq('buzz')
+      expect(subject.fizzbuzz(20)).to eq('buzz')
+    end
+
+    it "returns 'fizzbuzz' when N is divisible by both 3 and 5" do
+      expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(subject.fizzbuzz(30)).to eq('fizzbuzz')
+      expect(subject.fizzbuzz(45)).to eq('fizzbuzz')
+    end
+
+    it 'returns N as a string when N is not divisible by 3 or 5' do
+      expect(subject.fizzbuzz(7)).to eq('7')
+      expect(subject.fizzbuzz(11)).to eq('11')
+      expect(subject.fizzbuzz(13)).to eq('13')
+    end
+  end
 end
