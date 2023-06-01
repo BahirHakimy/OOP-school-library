561398aa238c0a5a810af6ef95c637cc868aeb6f
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-02 00:20:54 +0430

    Update solver.rb

--- src/tdd/solver.rb
+++ src/tdd/solver.rb
@@ -1,5 +1,6 @@
 class Solver
   def factorial(num)
+    raise ArgumentError, "Can't calculate factorial for negative numbers" if num.negative?
     return 1 if num <= 1
 
     num * factorial(num - 1)
